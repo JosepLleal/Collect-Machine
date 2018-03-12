6c8bc928fce71d546ce20646b8946471a1feb3b6
JoanBarduena <36154195+JoanBarduena@users.noreply.github.com>
2018-03-12 19:56:30 +0100

    Adding Include Mixer

--- collect-machine/Source.cpp
+++ collect-machine/Source.cpp
@@ -1,12 +1,13 @@
 
 #include "SDL/include/SDL.h"
 #include "SDL_Image/include/SDL_image.h"
-
+#include "SDL_Mixer/include/SDL_mixer.h"
 
 
 #pragma comment(lib, "SDL/libx86/SDL2.lib")
 #pragma comment(lib, "SDL/libx86/SDL2main.lib")
 #pragma comment(lib, "SDL_Image/libx86/SDL2_image.lib")
+#pragma comment(lib, "SDL_Mixer/libx86/SDL2_mixer.lib")
 
 
 
